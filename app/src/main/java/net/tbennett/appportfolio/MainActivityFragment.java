8a76d390c740012e635bc9e535d0872c6b500f77
Tim Bennett <bennetimo@gmail.com>
2015-10-31 01:33:58 +0000

    initial commit

---
+++ app/src/main/java/net/tbennett/appportfolio/MainActivityFragment.java
@@ -0,0 +1,25 @@
+package net.tbennett.appportfolio;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
